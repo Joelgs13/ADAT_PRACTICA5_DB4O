ac8c8a00104cb18893bf5be0455c413083ba4cc5
Joelgs13 <050213jgonzalez@gmail.com>
2024-11-27 18:38:26 +0100

    queda cargar los datos creando los daos antes de empezar con el primer punto

--- src/main/java/joel/adat/MenuOlimpiadas.java
+++ src/main/java/joel/adat/MenuOlimpiadas.java
@@ -31,7 +31,7 @@ public class MenuOlimpiadas {
         try {
             opcion = scanner.nextInt();
             if (opcion==1) {
-                cargarDatos();
+                cargarDatos(file,bbdd);
             }
         } catch (NumberFormatException e) {
             System.out.println("Supondre que no...");
@@ -54,8 +54,73 @@ public class MenuOlimpiadas {
         } while (opcion!=0);
     }
 
-    private static void cargarDatos() {
-        //x hacer
+    public static void cargarDatos(File ruta,ObjectContainer db) {
+        try (CSVReader reader = new CSVReader(new FileReader(ruta))) {
+            List<String[]> lineas = reader.readAll();
+            lineas.remove(0); // Remover cabecera
+            for (String[] linea : lineas) {
+                ModeloDeporte deporte=DaoDeporte.conseguirPorNombre(linea[12],db);
+                if(deporte==null) {
+                    deporte=new ModeloDeporte(linea[12]);
+                }
+                ModeloDeportista deportista=DaoDeportista.conseguirPorNombre(linea[1], db);
+                if(deportista==null) {
+                    float f;
+                    try {
+                        f=Float.parseFloat(linea[5]);
+                    }catch (NumberFormatException e) {
+                        f=0f;
+                    }
+                    int i;
+                    try {
+                        i=Integer.parseInt(linea[4]);
+                    }catch (NumberFormatException e) {
+                        i=0;
+                    }
+                    deportista=new ModeloDeportista(linea[1],linea[2].charAt(0), f, i);
+                }
+                ModeloEquipo equipo=DaoEquipo.conseguirPorNombre(linea[6], db);
+                if(equipo==null) {
+                    equipo=new ModeloEquipo(linea[6], linea[7]);
+                }
+                ModeloOlimpiada olimpiada=DaoOlimpiada.conseguirPorNombre(linea[8], db);
+                if(olimpiada==null) {
+                    int i;
+                    try {
+                        i=Integer.parseInt(linea[9]);
+                    }catch(NumberFormatException e) {
+                        i=0;
+                    }
+                    olimpiada=new ModeloOlimpiada(linea[8],i,linea[10],linea[11]);
+                }
+                ModeloEvento evento=DaoEvento.conseguirPorNombre(linea[13], db);
+                if(evento==null) {
+                    evento=new ModeloEvento(linea[13], olimpiada, deporte);
+                }
+                ModeloParticipacion participacion=DaoParticipacion.conseguirPorDeportistaEvento(deportista, evento, db);
+                if(participacion==null) {
+                    int i;
+                    try {
+                        i=Integer.parseInt(linea[3]);
+                    }catch(NumberFormatException e) {
+                        i=0;
+                    }
+                    participacion=new ModeloParticipacion(deportista, evento, equipo, i, linea[14]);
+                }
+                DaoDeporte.insertar(deporte, db);
+                DaoDeportista.insertar(deportista, db);
+                DaoEquipo.insertar(equipo, db);
+                DaoEvento.insertar(evento, db);
+                DaoOlimpiada.insertar(olimpiada, db);
+                DaoParticipacion.insertar(participacion, db);
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (CsvException e) {
+            e.printStackTrace();
+        }
     }
 
     private static File cargarArchivo(ModeloDeporte modeloDeporte) {
